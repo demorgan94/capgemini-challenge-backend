7d568e6485b548f68f49f0df418479c9f5a1576a
David Morgan <demorgan94@gmail.com>
2023-02-13 18:24:00 -0600

    Init repository

---
+++ src/main/java/examen/davidmorgan/CapgeminiChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package examen.davidmorgan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapgeminiChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapgeminiChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/examen/davidmorgan/CapgeminiChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package examen.davidmorgan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapgeminiChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
